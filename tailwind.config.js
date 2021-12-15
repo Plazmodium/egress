1f1fdbee6d859ba2f1175cefad12811c1d7dea12
Gabor Racz <gabzracz@icloud.com>
2021-12-15 13:50:18 +0200

    UI: CSS typerwriter

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   purge: { enabled: true, content: ["./src/**/*.{html,ts}"] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      letterSpacing: {
+        wide: ".15em",
+      },
+    },
   },
   variants: {
     extend: {},
