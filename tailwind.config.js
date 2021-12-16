7284d7e417cdd2a16ed271c09a77f84e2e3f0f52
Gabor Racz <gabzracz@icloud.com>
2021-12-16 12:53:15 +0200

    testing implem

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,24 @@ module.exports = {
       letterSpacing: {
         wide: ".15em",
       },
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 5s ease-out",
+        "fade-out-down": "fade-out-down 0.5s ease-out",
+        "fade-in-up": "fade-in-up 0.5s ease-out",
+        "fade-out-up": "fade-out-up 5s ease-out",
+      },
     },
   },
   variants: {
